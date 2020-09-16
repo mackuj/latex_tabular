939cd55107767875329e8d0b715e8791d6438e38
mackuj <macku.jakub.mi@gmail.com>
2020-09-16 12:25:20 +0200

    Add files via upload

---
+++ python_pro_fyziky.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+def addline(line, o, delimiter):
+    outputstr = ""
+    escape_chars = ["#", "$", "%", "&", "{", "}", "_", "~", "^", "\\"]
+    for char in line:
+        if char ==  delimiter:
+            outputstr += "&"
+        elif char in escape_chars:
+            outputstr += "\\"+char
+        else:
+            outputstr += char
+    outputstr += "\\\\ \\hline\n"
+    o.write(outputstr)
+
+
+def head_settup(line, o, delimiter):
+    colum_num = 0
+    colum_str = "c|"
+    for char in line:
+        if char ==  delimiter:
+            colum_num += 1
+    for i in range(colum_num):
+        colum_str += "c|"
+    o.write("\\begin{tabular}{|" + colum_str + "}\n")
+    o.write("\\hline\n")
+
+def end_settup(o):
+    o.write("\\end{tabular}\n")
+
+def newfilename(filename):
+    outputfile = ""
+    for char in filename:
+        if char != ".":
+            outputfile += char
+        else:
+            return outputfile + "_latex.txt"
+
+
+def analysis(filename, delimiter):
+    outputfile = newfilename(filename)
+    print(outputfile)
+    f = open(filename, "r")
+    o = open(outputfile, "w")
+    firstline = True
+
+    for line in f.readlines():
+        if firstline == True:
+            head_settup(line, o, delimiter)
+            addline(line, o, delimiter)
+            print("neco\n")
+            firstline = False
+            print("neco\n")
+        else:
+            addline(line, o, delimiter)
+    end_settup(o)
+
+    f.close()  
+    o.close()               
+
+def main():                                                          # Získání vstupu od uživatele
+    vstup_txt = input("Zadejte jméno souboru (cestu): ")
+    delimiter = input("a zadejte oddělovací znak: ")
+
+    analysis(vstup_txt, delimiter)
+
+if __name__ == '__main__':
+    main()
